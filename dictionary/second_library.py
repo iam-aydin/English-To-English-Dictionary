e4fe7eed0a2d3a1ad8da75ff8356ab1367726b12
AyDin VM <134310053+iam-aydin@users.noreply.github.com>
2023-08-30 14:19:31 +0330

    Add files via upload

---
+++ dictionary/second_library.py
@@ -0,0 +1,43 @@
+words_mean_2 = [
+    {
+         "associated": "(of a person or thing) connected with something else."
+    },
+    {
+         "triggered": "(of a mechanism) activated by a trigger"
+    },
+    {
+         "constructing": "build or make (something, typically a building, road, or machine)."
+    },
+    {
+         "constructor": "a person or company that builds, designs, or makes something, a subroutine designed to create "
+                        "an object in a particular class."
+    },
+    {
+         "character": "the mental and moral qualities distinctive to an individual."
+    },
+    {
+         "aim": "a purpose or intention; a desired outcome, the directing of a weapon or missile at a target"
+    },
+    {
+         "objective": "a thing aimed at or sought; a goal"
+    },
+
+    {
+         "i.e": "for instance"
+    },
+    {
+         "e.g": "for example"
+    },
+    {
+        "resident evil": "A game series from CAPCOM Corporation"
+    },
+    {
+        "trigger": "a small device that releases a spring or catch and so sets off a mechanism, especially in order to fire a gun, cause (a device) to function."
+    },
+    {
+        "bind": "stick together or cause to stick together in a single mass."
+    },
+    {
+        "binding": "having power to bind or oblige; obligatory"
+    }
+]
